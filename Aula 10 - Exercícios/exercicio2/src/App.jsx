6b340b5520db1e512faf9ae6a7d8d12b2ecd7203
BrunaElohaBonk <brunaelohabonk698@gmail.com>
2025-09-04 10:25:29 -0300

    Fiz o exercício 2

--- Aula 10 - Exercícios/exercicio2/src/App.jsx
+++ Aula 10 - Exercícios/exercicio2/src/App.jsx
@@ -4,28 +4,66 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { useEffect } from 'react'
 
-function App() {
 //   2. Temporizador regressivo
 //  Input para o usuário digitar um número (em segundos).
 //  Botão Iniciar começa a contagem regressiva.
 //  Quando chegar em 0, exibir “⏰ Tempo esgotado!”.
 
+function App() {
   const [num, setNum] = useState(0)
+  const [isRunning, setIsRunning] = useState(false) // estado que controla se o timer está ativo
+  const [finished, setFinished] = useState(false)   // diz se o tempo acabou
 
   useEffect(() => {
-    // Atualiza a cada segundo
-    const intervalo = setInterval(() => {
-      setNum(num -1);
-    }, 1000);""
-  }, []); 
+    let intervalo;
+
+    if (isRunning && num > 0) { // se o timer estiver ativo e num for maior que 0
+      intervalo = setInterval(() => {
+        setNum(prevNum => {
+          if (prevNum <= 1) {
+            clearInterval(intervalo)   // para o intervalo
+            setIsRunning(false)         // timer para
+            setFinished(true)           // marca que acabou
+            return 0
+          }
+          return prevNum - 1            // prevNum significa: "Atualiza num para o valor anterior menos 1"
+        })
+      }, 1000)
+    }
+
+    // limpa o intervalo quando componente desmontar ou mudar dependências
+    return () => clearInterval(intervalo)
+
+  }, [isRunning, num])
+
+  function iniciar() {
+    if (num > 0) {
+      setIsRunning(true)   
+      setFinished(false)   
+    }
+  }
 
   return (
     <>
-      <span className='titulo'>Temporizador regressivo</span>
+      <h1>Temporizador regressivo</h1>
+
+      <input
+        type="number"
+        placeholder="Digite um número"
+        onChange={e => setNum(Number(e.target.value))}
+        disabled={isRunning}  // bloqueia input durante contagem
+      />
+
       <br /><br />
-      <input type="text" placeholder='Digite um número:' onChange={(e) => setNum(e.target.value)} />
-      <br /><br />
-      <button className='button' onClick={useEffect}>Iniciar</button>
+
+      <button className='button' onClick={iniciar} disabled={isRunning || num <= 0}>
+        Iniciar
+      </button>
+
+      <div style={{ marginTop: 20, fontSize: 30, fontWeight: 'bold' }}>
+        {isRunning && num > 0 && num}
+        {!isRunning && finished && "⏰ Tempo esgotado!"}
+      </div>
     </>
   )
 }
