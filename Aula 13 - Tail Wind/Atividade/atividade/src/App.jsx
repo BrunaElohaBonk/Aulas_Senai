18da161eb73c43f1d2825cb6f5f5abae4b62f3eb
BrunaElohaBonk <brunaelohabonk698@gmail.com>
2025-09-18 11:42:25 -0300

    Fiz uma parte da atividade

--- Aula 13 - Tail Wind/Atividade/atividade/src/App.jsx
+++ Aula 13 - Tail Wind/Atividade/atividade/src/App.jsx
@@ -8,26 +8,28 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="h-screen">
+        <div className="h-1/3 flex justify-between">
+          <div className="w-52 h-52 bg-blue-500">
+            <div className="h-1/3 flex justify-center bg-amber-500 w-18"></div>
+            <div className="h-1/3"></div>
+            <div className="h-1/3"></div>
+          </div>
+          <div className="w-52 h-52 bg-blue-500"></div>
+          <div className="w-52 h-52 bg-blue-500"></div>
+          <div className="w-52 h-52 bg-blue-500"></div>
+        </div>
+        <div className="h-1/3 flex justify-between">
+          <div className="w-52 h-52 bg-blue-500"></div>
+          <div className="w-52 h-52 bg-blue-500"></div>
+          <div className="w-52 h-52 bg-blue-500"></div>
+        </div>
+        <div className="h-1/3 flex justify-between">
+          <div className="w-52 h-52 bg-blue-500"></div>
+          <div className="w-52 h-52 bg-blue-500"></div>
+          <div className="w-52 h-52 bg-blue-500"></div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
