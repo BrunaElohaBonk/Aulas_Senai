834b0d8e7208ab836e930cbf4e0ba5195735ce0c
BrunaElohaBonk <brunaelohabonk698@gmail.com>
2025-09-24 08:50:21 -0300

    Terminei a atividade

--- Aula 13 - Tail Wind/Atividade/atividade/src/App.jsx
+++ Aula 13 - Tail Wind/Atividade/atividade/src/App.jsx
@@ -11,23 +11,90 @@ function App() {
       <div className="h-screen">
         <div className="h-1/3 flex justify-between">
           <div className="w-52 h-52 bg-blue-500">
-            <div className="h-1/3 flex justify-center bg-amber-500 w-18"></div>
+            <div className="h-1/3 flex justify-center">
+              <div className="bg-amber-500 w-18"></div>
+            </div>
+            <div className="h-1/3"></div>
+            <div className="h-1/3 flex justify-center">
+              <div className="bg-amber-500 w-18"></div>
+            </div>
+          </div>
+          <div className="w-52 h-52 bg-blue-500">
+            <div className="h-1/3"></div>
+            <div className="h-1/3 flex justify-center">
+              <div className="bg-amber-500 w-18"></div>
+            </div>
+            <div className="h-1/3"></div>
+          </div>
+          <div className="w-52 h-52 bg-blue-500">
+            <div className="h-1/3 flex ">
+              <div className="bg-amber-500 w-18"></div>
+            </div>
+            <div className="h-1/3"></div>
+            <div className="h-1/3 flex justify-end">
+              <div className="bg-amber-500 w-18"></div>
+            </div>
+          </div>
+          <div className="w-52 h-52 bg-blue-500">
+            <div className="h-1/3"></div>
+            <div className="h-1/3"></div>
+            <div className="h-1/3 flex justify-center">
+              <div className="bg-amber-500 w-18"></div>
+            </div>
+          </div>
+        </div>
+        <div className="h-1/3 flex justify-between">
+          <div className="w-52 h-52 bg-blue-500">
+            <div className="h-1/3 flex justify-center">
+              <div className="bg-amber-500 w-18"></div>
+            </div>
             <div className="h-1/3"></div>
             <div className="h-1/3"></div>
           </div>
-          <div className="w-52 h-52 bg-blue-500"></div>
-          <div className="w-52 h-52 bg-blue-500"></div>
-          <div className="w-52 h-52 bg-blue-500"></div>
+          <div className="w-52 h-52 bg-blue-500">
+            <div className="h-1/3 flex justify-center">
+              <div className="bg-amber-500 w-18"></div>
+            </div>
+            <div className="h-1/3 flex justify-between">
+              <div className="bg-amber-500 w-18"></div>
+              <div className="bg-amber-500 w-18"></div>
+            </div>
+            <div className="h-1/3 flex justify-center">
+              <div className="bg-amber-500 w-18"></div>
+            </div>
+          </div>
+          <div className="w-52 h-52 bg-blue-500">
+            <div className="h-1/3 flex justify-center">
+              <div className="bg-amber-500 w-18"></div>
+            </div>
+            <div className="h-1/3"></div>
+            <div className="h-1/3"></div>
+          </div>
         </div>
         <div className="h-1/3 flex justify-between">
-          <div className="w-52 h-52 bg-blue-500"></div>
-          <div className="w-52 h-52 bg-blue-500"></div>
-          <div className="w-52 h-52 bg-blue-500"></div>
-        </div>
-        <div className="h-1/3 flex justify-between">
-          <div className="w-52 h-52 bg-blue-500"></div>
-          <div className="w-52 h-52 bg-blue-500"></div>
-          <div className="w-52 h-52 bg-blue-500"></div>
+          <div className="w-52 h-52 bg-blue-500">
+            <div className="h-1/3 flex justify-end">
+              <div className="bg-amber-500 w-18"></div>
+            </div>
+            <div className="h-1/3"></div>
+            <div className="h-1/3 flex">
+              <div className="bg-amber-500 w-18"></div>
+            </div>
+          </div>
+          <div className="w-52 h-52 bg-blue-500">
+            <div className="h-1/3"></div>
+            <div className="h-1/3 flex justify-center">
+              <div className="bg-amber-500 w-18"></div>
+            </div>
+            <div className="h-1/3"></div>
+          </div>
+          <div className="w-52 h-52 bg-blue-500">
+            <div className="h-1/3 flex justify-end">
+              <div className="bg-amber-500 w-18"></div>
+            </div>
+            <div className="h-1/3"></div>
+            <div className="h-1/3"></div>
+          </div>
         </div>
       </div>
     </>
