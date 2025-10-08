ba1e848b00366505f3e11ae4d3a045df4d0b3a8e
BrunaElohaBonk <brunaelohabonk698@gmail.com>
2025-10-08 11:23:07 -0300

    Fiz mais algumas modificações na atividade

--- Aula 16 - Atividades/aula16/src/pages/List.jsx
+++ Aula 16 - Atividades/aula16/src/pages/List.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 import { Router, useNavigate } from 'react-router-dom'
+import './List.css'
 
 function List() {
   const [personagem, setPersonagem] = useState([])
@@ -23,13 +24,28 @@ function List() {
 
   return (
     <>
-    {
-      personagem.map(personagem => (
-          <li key={personagem._id} style={{ cursor: "pointer", marginBottom: "8px"}} onClick={() => openPersonagem(personagem)}>
-            {personagem.name}
-          </li>  
-        ))
-    }
+    <div className=" body">
+      <div className='flex faixa'>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/8/8f/Disney_logo.png" className='disney' alt="disney" height="30"/>
+      </div>
+
+      {
+        personagem.map(personagem => (
+          <div className="container">
+            <div>
+                {
+                  personagem._id > 25 ? "" : <li key={personagem._id} style={{ cursor: "pointer", marginBottom: "8px" }} onClick={() => openPersonagem(personagem)}> {personagem.name}</li> 
+                }
+            </div>
+            <div>
+                {
+                  personagem._id < 25 ? "" : <li key={personagem._id} style={{ cursor: "pointer", marginBottom: "8px" }} onClick={() => openPersonagem(personagem)}> {personagem.name}</li> 
+                }
+            </div>
+          </div>
+          ))
+      }
+    </div>
     </>
   )
 }
