c0f06be0ea49f3ca65dc8bb0bc2936a8bcc543e0
BrunaElohaBonk <brunaelohabonk698@gmail.com>
2025-10-09 11:40:22 -0300

    Fiz a revisão da aula de hj

---
+++ Aula 17 - Revisão/Revisao/src/App.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react"
+
+function Pessoa({nome, sobrenome, idade}){
+  return(
+    <>
+    <h1>Nome: {nome}</h1>
+    <h1>Sobrenome: {sobrenome}</h1>
+    <h1>Idade: {idade}</h1>
+    </>
+  )     
+}
+
+function App() {
+  const [value, setValue] = useState(0)
+  const [text, setText] = useState("")
+  const [show, setShow] = useState(true)
+  
+  useEffect(() => {
+    console.log("Testando o useEffect com Text")
+  }, [text])
+
+  useEffect(() => {
+    console.log("Testando o useEffect com Value")
+  }, [value])
+
+  return (
+    <>
+      <button onClick={() => setValue(value + 1)}>+</button>
+      {value}
+      <button onClick={() => setValue(value - 1)}>-</button>
+
+      <br />
+
+      <input type="text" onChange={(e) => setText(e.target.value)}/>
+      <h1>{text}</h1>
+
+      <br />
+
+      <button onClick={() => setShow(!show)}>Alternar visibilidade</button>
+      { show &&
+        <div style={{backgroundColor: 'blue', width: '200px', height: '200px'}}></div>
+      }
+
+      <Pessoa nome = "Bruna Elohá" sobrenome = 'Bonk' idade = '19'/>
+    </>
+  )
+}
+
+
+
+export default App
